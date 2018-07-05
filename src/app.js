c5ba168b6c4c20f7764b66f9183c7924e2bde410
Jyoti Singh <jyotisingh7@gmail.com>
2018-07-05 12:37:15 +0530

    wip

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-
 const axios = require('axios')
 const url = 'http://checkip.amazonaws.com/';
+const NodeRSA = require('node-rsa');
 let response;
 
 
 exports.lambda_handler = async (event, context, callback) => {
     try {
+const key = new NodeRSA({b: 512});
+ 
+const text = 'Hello RSA!';
+const encrypted = key.encrypt(text, 'base64');
+console.log('encrypted: ', encrypted);
+const decrypted = key.decrypt(encrypted, 'utf8');
+console.log('decrypted: ', decrypted);
+
         const ret = await axios(url);
         response = {
             'statusCode': 200,
